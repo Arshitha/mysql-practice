6064720d811e4c80760e209463705a03a3f19632
Arshitha Basavaraj <arsh2794@gmail.com>
2018-08-06 11:30:19 -0400

    solutions to http://sqlzoo.net/ tuturial questions

---
+++ sqlzoo/nobel.sql
@@ -0,0 +1,34 @@
+
+--#1
+/*The example uses a WHERE clause to show the population of 'France'. 
+Note that strings (pieces of text that are data) should be in 'single quotes';*/
+-- Modify it to show the population of Germany
+
+
+--#2
+/**/
+
+--#3
+/**/
+
+--#4
+/**/
+
+--#5
+/**/
+
+--#6
+/**/
+
+--#7
+/**/
+
+--#8
+/**/
+
+--#9
+/**/
+
+--10
+/**/
+
